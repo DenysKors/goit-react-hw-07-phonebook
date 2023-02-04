b75e089ee4bde4e7379738ad734d56c66c01bbc6
DenysKors <105918404+DenysKors@users.noreply.github.com>
2023-02-04 18:14:15 +0200

    ContactItem & styles

--- src/components/ContactItem/ContactItem.styled.jsx
+++ src/components/ContactItem/ContactItem.styled.jsx
@@ -1,15 +1,31 @@
 import styled from 'styled-components';
 
+export const Item = styled.li`
+  padding-left: 5px;
+  padding-right: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 40px;
+  border: 2px solid rgba(33, 33, 33, 0.1);
+  border-radius: 4px;
+
+  &:not(:last-child) {
+    margin-bottom: 5px;
+  }
+`;
+
 export const Text = styled.p`
-  margin-right: 20px;
   display: inline-block;
 
   font-weight: 500;
-  font-size: 16px;
+  font-size: 18px;
+  line-height: 1.18;
 `;
 
 export const Button = styled.button`
   width: 70px;
+  height: 30px;
   padding: 3px;
   font-weight: 500;
   font-size: 14px;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,26 +1,9 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { selectFilteredContacts } from 'redux/selectors';
-import { Text, Button } from 'components/ContactList/ContactList.styled';
-import { deleteContact } from 'redux/contactsThunk';
+import { ContactItem } from 'components/ContactItem/ContactItem';
 
 export const ContactList = () => {
-  const dispatch = useDispatch();
-
-  const filteredContacts = useSelector(selectFilteredContacts);
-
-  const onDeleteContact = id => {
-    dispatch(deleteContact(id));
-  };
   return (
-    <ul style={{ width: 500 }}>
-      {filteredContacts.map(({ id, name, phone }) => (
-        <li key={id}>
-          <Text>
-            {name}: {phone}
-          </Text>
-          <Button onClick={() => onDeleteContact(id)}>Delete</Button>
-        </li>
-      ))}
+    <ul>
+      <ContactItem />
     </ul>
   );
 };
